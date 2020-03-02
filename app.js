2bc5776bad446d48ebeebb5b09935ec9b2ccc68d
Joshua Jakab <joshua.jakab@gmail.com>
2020-03-02 13:52:18 -0500

    server

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('.'))
+
+app.get('/', (req, res) => {
+    res.sendFile("index.html")
+})
 
 app.listen(PORT, () => {console.log(`Running on port: ${PORT}`)})
